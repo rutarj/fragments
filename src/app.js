83aeb12db7f9ee071abf2d018c6fedae538720dc
Rutarj <shahrutarj@gmail.com>
2024-05-20 23:17:48 -0400

    Updated app.js to use new authentication strategy and initialized Passport.js, also modified API routes

--- src/app.js
+++ src/app.js
@@ -3,9 +3,12 @@ const cors = require('cors');
 const helmet = require('helmet');
 const compression = require('compression');
 
+const passport = require('passport');
+
+const authenticate = require('./auth');
 // author and version from our package.json file
 // TODO: make sure you have updated your name in the `author` section
-const { author, version } = require('../package.json');
+//const { author, version } = require('../package.json');
 
 const logger = require('./logger');
 const pino = require('pino-http')({
@@ -17,19 +20,6 @@ const pino = require('pino-http')({
 const app = express();
 // modifications to src/app.js
 
-const passport = require('passport');
-
-const authenticate = require('./auth');
-
-// Use gzip/deflate compression middleware
-app.use(compression());
-
-// Set up our passport authentication middleware
-passport.use(authenticate.strategy());
-app.use(passport.initialize());
-
-// Define our routes
-app.use('/', require('./routes'));
 
 // Use pino logging middleware
 app.use(pino);
@@ -43,22 +33,12 @@ app.use(cors());
 // Use gzip/deflate compression middleware
 app.use(compression());
 
-// Define a simple health check route. If the server is running
-// we'll respond with a 200 OK.  If not, the server isn't healthy.
-app.get('/', (req, res) => {
-  // Clients shouldn't cache this response (always request it fresh)
-  // See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Caching#controlling_caching
-  res.setHeader('Cache-Control', 'no-cache');
+// Set up our passport authentication middleware
+passport.use(authenticate.strategy());
+app.use(passport.initialize());
 
-  // Send a 200 'OK' response with info about our repo
-  res.status(200).json({
-    status: 'ok',
-    author,
-    // TODO: change this to use your GitHub username!
-    githubUrl: 'https://github.com/rutarj/fragments.git',
-    version,
-  });
-});
+// Define our routes
+app.use('/', require('./routes'));
 
 // Add 404 middleware to handle any requests for resources that can't be found
 app.use((req, res) => {
--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -5,7 +5,6 @@
  */
 const express = require('express');
 
-
 // Create a router on which to mount our API endpoints
 const router = express.Router();
 
